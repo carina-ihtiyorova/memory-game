86b7268deab05bdeb7f6c1965a30e45ddb263fd5
Nursultan <nursultan_kas@icloud.com>
2024-05-31 00:52:12 -0400

    jfndfnadsl

--- memory-game/src/Hooks/useGetImg.jsx
+++ memory-game/src/Hooks/useGetImg.jsx
@@ -1,1 +1,27 @@
-export default useGe
+import axios from "axios"
+
+export default useGetImg = async ()=>{
+    const options = {
+        method: 'GET',
+        url: 'https://ronreiter-meme-generator.p.rapidapi.com/meme',
+        params: {
+          top: 'Top Text',
+          bottom: 'Bottom Text',
+          meme: 'Condescending-Wonka',
+          font_size: '50',
+          font: 'Impact'
+        },
+        headers: {
+          'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+          'X-RapidAPI-Host': 'ronreiter-meme-generator.p.rapidapi.com'
+        }
+      };
+
+
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
