cdad0e5fe61e1311df4255250d1428559aedbcca
Nursultan <nursultan_kas@icloud.com>
2024-05-31 00:43:16 -0400

    Create Board

---
+++ memory-game/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './Components/Board';
+
+export default function App() {
+
+    
+
+
+    return (
+     <div className='w-[100vw] h-[100vh] bg-red-600'>
+       <Board/>
+     </div>
+    )
+  }
+
+
